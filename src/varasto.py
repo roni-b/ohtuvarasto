3a05604fa416359836000657d811369c66bfde13
Roni Bärman <104189902+roni-b@users.noreply.github.com>
2023-11-08 12:50:33 +0200

    Add pylint

--- src/varasto.py
+++ src/varasto.py
@@ -1,5 +1,15 @@
 class Varasto:
+    """
+    Luokka mallintaa varastoa ja tarjoaa metodit sen käsittelyyn.
+    """
     def __init__(self, tilavuus, alku_saldo = 0):
+        """
+        Varaston konstruktori.
+
+        Args:
+            tilavuus (float): Varaston tilavuus.
+            alku_saldo (float, optional): Alkusaldo varastolle.
+        """
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
         else:
@@ -16,11 +26,22 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
+        """
+        Palauttaa määrän, kuinka paljon tavaraa varastoon mahtuu.
+
+        Returns:
+            float: Määrä, kuinka paljon tavaraa mahtuu varastoon.
+        """
         return self.tilavuus - self.saldo
 
     def lisaa_varastoon(self, maara):
+        """
+        Lisää tavaraa varastoon.
+
+        Args:
+            maara (float): Määrä, joka lisätään varastoon.
+        """
         if maara < 0:
             return
         if maara <= self.paljonko_mahtuu():
@@ -29,6 +50,15 @@ class Varasto:
             self.saldo = self.tilavuus
 
     def ota_varastosta(self, maara):
+        """
+        Ottaa tavaraa varastosta.
+
+        Args:
+            maara (float): Määrä, joka otetaan varastosta.
+
+        Returns:
+            float: Palauttaa otetun määrän, tai 0.0.
+        """
         if maara < 0:
             return 0.0
         if maara > self.saldo:
@@ -42,4 +72,10 @@ class Varasto:
         return maara
 
     def __str__(self):
+        """
+        Palauttaa varaston tilan merkkijonona.
+
+        Returns:
+            str: Varaston tila muodossa "saldo = <saldo>, vielä tilaa <tila>".
+        """
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
